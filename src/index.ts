e8e1b3ad7b4fd2311d906d74314eab72d3d07432
Marian Lorinc <mlorinc@redhat.com>
2022-10-24 17:04:02 +0200

    Export ProvideFunction and BaseContext
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { declareDependencies, StateMachine } from './StateMachine';
+export { declareDependencies, StateMachine, BaseContext } from './StateMachine';
 export { State, StateData } from './State';
 export { CriticalError, TimeoutError } from './Error';
 export * from './WebElementFunctions';
 export { Dependency, ValueDependency } from './Dependency';
 export { WebElementDependency } from './WebElementDependency';
-export { ProvidePublic, ProvideNothing, ProvideComplete } from './Provide';
+export { ProvidePublic, ProvideNothing, ProvideComplete, ProvideFunction } from './Provide';
 export { logger } from './Logger';
